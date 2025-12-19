d5adea586dc7874ab243448442fe5f1ed5de7f11
丞竣 <jmj987654321@gmail.com>
2025-12-19 22:53:54 +0800

    support local and render firebase credentials

--- myapp/firebase_init.py
+++ myapp/firebase_init.py
@@ -1,9 +1,15 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate(
-    "shopping-54704-firebase-adminsdk-fbsvc-abdc0effc3.json"
-)
+if not firebase_admin._apps:
+    if os.getenv("RENDER"):  
+        # Render 環境
+        cred = credentials.Certificate("/etc/secrets/firebase.json")
+    else:
+        # 本機開發環境
+        cred = credentials.Certificate("shopping-54704-firebase-adminsdk-fbsvc-abdc0effc3.json")
 
-firebase_admin.initialize_app(cred)
+    firebase_admin.initialize_app(cred)
+
 db = firestore.client()
